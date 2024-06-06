dd462a3722865391e020c8a562afde9310fc3a65
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-06-06 11:48:41 -0500

    Initial Commit

---
+++ ColorIconic/ColorIconicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorIconicApp.swift
+//  ColorIconic
+//
+//  Created by Sravanthi Chinthireddy on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorIconicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
