2d33551bebe263f20a6b27cada099ba1f99f26c0
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-06-17 15:06:51 -0500

    - Swift, SwiftUI logo design using Path and Shape

---
+++ ColorIconic/LogoView.swift
@@ -0,0 +1,18 @@
+//
+//  LogoView.swift
+//  ColorIconic
+//
+//  Created by Sravanthi Chinthireddy on 12/06/24.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoView()
+}
---
+++ ColorIconic/SwiftLogoPathView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftLogoPathView.swift
+//  ColorIconic
+//
+//  Created by Sravanthi Chinthireddy on 12/06/24.
+//
+
+import SwiftUI
+
+struct SwiftLogoPathView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftLogoPathView()
+}
